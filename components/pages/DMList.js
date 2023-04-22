6b1157707479ce10fd7506a1cec2635383e6e171
Minseok Song <minseoksong8@gmail.com>
2023-04-22 15:33:02 -0700

    added sample data, placeholder

---
+++ components/pages/DMList.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
+
+const sampleData = [
+  {
+    id: 1,
+    name: 'Arnold S.',
+    profileImg: 'IMG1',
+    body: 'Love your biceps bruh',
+  },
+  {
+    id: 2,
+    name: 'Ronnie C',
+    profileImg: 'IMG2',
+    body: 'Gummy or brownie',
+  },
+  {
+    id: 3,
+    name: 'Swolebrrrah',
+    profileImg: 'IMG3',
+    body: 'I like the limited edition to Khaki and army green...',
+  },
+];
+
+const MessageList = () => {
+
+  return (
+<Text>DMList</Text>
+  );
+};
+
+export default MessageList;
