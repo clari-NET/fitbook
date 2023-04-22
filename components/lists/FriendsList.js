7d41419d0ca3df8911c93cdfc81026bdd9c84ebb
Andrew Ihn <andrewihn11@gmail.com>
2023-04-22 15:36:14 -0700

    Friends components created

---
+++ components/lists/FriendsList.js
@@ -0,0 +1,12 @@
+// import Card from '../cards/';
+
+export default function FriendsList({ friends }) {
+
+  return (
+    <ScrollView>
+      <View>
+        <Text>Hello from FriendsList!</Text>
+      </View>
+    </ScrollView>
+  )
+}
