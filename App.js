4da52e4e6d8b8afdb16cf93634e3e107ece1bb32
NicMilli <nicbass2@gmail.com>
2023-04-22 15:56:37 -0700

    React paper added with themes

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
 import { store } from './store/store'
 import { Provider } from 'react-redux'
-import { NavigationContainer } from '@react-navigation/native';
+import { Provider as PaperProvider, adaptNavigationTheme } from 'react-native-paper';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Fitbook from './Fitbook';
 import SignUp from './components/pages/SignUp'
+
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+} from 'react-native-paper';
+import merge from 'deepmerge';
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
+const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
 
 // Create Stack.Navigator component
 const Stack = createNativeStackNavigator();
@@ -11,12 +30,14 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Fitbook" component={Fitbook} />
-          <Stack.Screen name="SignUp" component={SignUp} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PaperProvider theme={CombinedDarkTheme}>
+        <NavigationContainer theme={CombinedDarkTheme}>
+          <Stack.Navigator initialRouteName="Fitbook">
+            <Stack.Screen name="Fitbook" component={Fitbook} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
     </Provider>
   );
 }
---
+++ components/pages/Landing.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import SignUp from './SignUp';
+
+export default function Landing({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, Fit Bros/Sis!</Text>
+      <Button
+        title="Go to Sign-up Screen"
+        onPress={() => {
+          navigation.navigate('SignUp')
+        }}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
