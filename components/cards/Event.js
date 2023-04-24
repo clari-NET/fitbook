01ea25f315f78804ed72ba9f673da83143d26f6f
Johnathan Simeroth <john.simeroth@gmail.com>
2023-04-24 12:45:43 -0700

    added rough placeholder of events carousel

--- components/cards/Event.js
+++ components/cards/Event.js
@@ -1,11 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Avatar, Button, Card, Text } from 'react-native-paper';
 
-export default function Event ({ event }) {
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+
+export default function Event({ event }) {
 
   return (
-    <View >
-      <Text >Hello from Event Card!</Text>
-    </View>
-  )
+    <Card>
+      <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+      <Card.Content>
+        <Text variant="titleLarge">Card title</Text>
+        <Text variant="bodyMedium">Card content</Text>
+      </Card.Content>
+      <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+      <Card.Actions>
+        <Button>Cancel</Button>
+        <Button>Ok</Button>
+      </Card.Actions>
+    </Card>
+  );
 }
--- components/lists/EventList.js
+++ components/lists/EventList.js
@@ -1,13 +1,22 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, Dimensions } from "react-native"
+import Carousel from 'react-native-snap-carousel';
 import Event from '../cards/Event.js';
+
+export const SLIDER_WIDTH = Dimensions.get('window').width + 80
+export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7)
 
 export default function EventList ({ events }) {
 
   return (
-    <View >
-      <Text >Hello from Events List!</Text>
-      {new Array(3).fill(0).map(event => <Event event={event}/>)}
-    </View>
+    <Carousel
+      data={events}
+      renderItem={Event}
+      sliderWidth={SLIDER_WIDTH}
+      itemWidth={ITEM_WIDTH}
+    />
+    //   <Text >Hello from Events List!</Text>
+    //   {new Array(3).fill(0).map(event => <Event event={event}/>)}
+    // </View>
   )
 }
