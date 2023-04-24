d30ef6c00a21f8f0dbf93bf1597d60aab7642c0e
Yale Yang <yalerkm25@gmail.com>
2023-04-24 16:41:25 -0700

    stop yelling at me linter

--- components/pages/Conversation.js
+++ components/pages/Conversation.js
@@ -2,35 +2,6 @@ import React, {useState, useEffect} from 'react';
 import { View, TouchableOpacity, StyleSheet} from 'react-native';
 import { Text, useTheme } from 'react-native-paper';
 
-export default function Conversation ({ currConvo, setCurrConvo }) {
-
-  const { colors } = useTheme();
-
-  return (
-    <>
-      <TouchableOpacity
-          onPress={() => {
-            console.log('back button was pressed');
-            setCurrConvo("DMList")
-          }}>
-        <Text>Back Button</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-          onPress={() => {
-            console.log('profile img was pressed');
-          }}>
-        <Text>{currConvo}'s profile img </Text>
-      </TouchableOpacity>
-      <View style={[styles.container, { backgroundColor: colors.surface }]}>
-        <Text >Conversation Page</Text>
-      </View>
-      <View>
-        <Text>Keyboard</Text>
-      </View>
-    </>
-  )
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -38,3 +9,36 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function Conversation({ currConvo, setCurrConvo }) {
+  const { colors } = useTheme();
+
+  return (
+    <>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('back button was pressed');
+          setCurrConvo('DMList');
+        }}
+      >
+        <Text>Back Button</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('profile img was pressed');
+        }}
+      >
+        <Text>
+          profile img:
+          {currConvo}
+        </Text>
+      </TouchableOpacity>
+      <View style={[styles.container, { backgroundColor: colors.surface }]}>
+        <Text>Conversation Page</Text>
+      </View>
+      <View>
+        <Text>Keyboard</Text>
+      </View>
+    </>
+  );
+}
--- components/pages/DMList.js
+++ components/pages/DMList.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FlatList, Image, StyleSheet, View, TouchableOpacity } from 'react-native';
 import { Text, useTheme } from 'react-native-paper';
-import Conversation from './Conversation.js';
+import Conversation from './Conversation';
 
 const sampleData = [
   {
@@ -24,44 +24,6 @@ const sampleData = [
   },
 ];
 
-export default function MessageList() {
-  const [messages, setMessages] = useState(sampleData);
-  const [currConvo, setCurrConvo] = useState("DMList");
-  const { colors } = useTheme();
-
-  const renderMessage = ({ item }) => (
-    <>
-      <TouchableOpacity
-          onPress={() => {
-            console.log(item.id, ' profile img was pressed');
-          }}>
-        <Text>profile img </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-          onPress={() => {
-            console.log(item.id, ' text was pressed');
-            setCurrConvo(item.name.toString());
-          }}>
-        <View style={[styles.container, { backgroundColor: colors.surface }]}>
-          <Text >{item.name}</Text>
-          <Text >{item.body}...</Text>
-        </View>
-      </TouchableOpacity>
-    </>
-  );
-
-  return currConvo === "DMList" ?
-  (
-    <FlatList
-      data={messages}
-      renderItem={renderMessage}
-      keyExtractor={item => item.id.toString()}
-    />
-  )
-  :
-  <Conversation currConvo={currConvo} setCurrConvo={setCurrConvo}/>
-};
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -69,3 +31,43 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default function MessageList() {
+  const [messages, setMessages] = useState(sampleData);
+  const [currConvo, setCurrConvo] = useState('DMList');
+  const { colors } = useTheme();
+
+  const renderMessage = ({ item }) => (
+    <>
+      <TouchableOpacity
+        onPress={() => {
+          console.log(item.id, ' profile img was pressed');
+        }}
+      >
+        <Text>profile img </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          console.log(item.id, ' text was pressed');
+          setCurrConvo(item.name.toString());
+        }}
+      >
+        <View style={[styles.container, { backgroundColor: colors.surface }]}>
+          <Text>{item.name}</Text>
+          <Text>
+            {item.body}
+            ...
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </>
+  );
+
+  return currConvo === 'DMList' ? (
+    <FlatList
+      data={messages}
+      renderItem={renderMessage}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  ) : <Conversation currConvo={currConvo} setCurrConvo={setCurrConvo} />;
+}
