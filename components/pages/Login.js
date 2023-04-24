30e0299d90d6febffb166057249b758c4a9d24e1
NicMilli <nicbass2@gmail.com>
2023-04-24 12:06:21 -0700

    Sing-up and login structure created

--- components/pages/Login.js
+++ components/pages/Login.js
@@ -1,22 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-// import SignUp from './components/pages/SignUp';
-import { useTheme } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { useTheme, Button, Text, Snackbar } from 'react-native-paper';
+import React, { useState } from 'react';
+// import {getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 
 export default function Login({ navigation }) {
   const { colors } = useTheme();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [failure, setFailure] = useState(false);
+
+  const handleLogin = async (e) =>{
+    e.preventDefault()
+
+    try {
+    const auth = getAuth()
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+    if(userCredential.user) {
+      navigation.navigate('Main')
+    }
+
+    const name = await auth.currentUser.displayName
+    toast.success(`Welcome back ${name}`)
+    setSuccess(true);
+    } catch (error) {
+        console.log(error)
+        toast.error('Incorrect user credentials')
+    }
+}
 
   return (
+    <>
+    <Snackbar
+        visible={visible}
+        onDismiss={onDismissSnackBar}
+        action={{
+          label: 'Undo',
+          onPress: () => {
+            // Do something
+          },
+        }}>
+        Hey there! I'm a Snackbar.
+      </Snackbar>
     <View style={[styles.container, { backgroundColor: colors.surface }]}>
       <Text>Hello, Fit Bros/Sis!</Text>
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={email => setUsername(email)}
+        mode="outlined"
+        style={styles.input}
+      />
+      <TextInput
+        label="Password"
+        value={password}
+        onChangeText={password => setPassword(password)}
+        mode="outlined"
+        style={styles.input}
+      />
       <Button
-        title="Go to Sign-up Screen"
+        mode="elevated"
+        buttonColor={colors.primary}
+        textColor={colors.secondary}
+        onPress={() => {
+          console.log('Create handleLogin')
+        }}
+      >Login</Button>
+      <Text>Need to create an account?</Text>
+      <Button
+        mode="outlined"
+        textColor={colors.secondary}
         onPress={() => {
           navigation.navigate('SignUp')
         }}
-      />
+      >Sign-up</Button>
       <StatusBar style="auto" />
     </View>
+    </>
+
   );
 }
 
@@ -25,5 +89,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
   },
 });
--- components/pages/SignUp.js
+++ components/pages/SignUp.js
@@ -1,24 +1,55 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { useTheme } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { useTheme, TextInput, Button, Text } from 'react-native-paper';
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function SignUp({ navigation }) {
   const { colors } = useTheme();
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <View style={[styles.container, { backgroundColor: colors.surface }]}>
       <Text>Sign up, Swole Bro/Sis!</Text>
-      <Button
-        title="Go Back"
-        onPress={() => {
-          navigation.goBack();
-        }}
+      <TextInput
+        label="Username"
+        value={username}
+        onChangeText={username => setUsername(username)}
+        mode="outlined"
+        style={styles.input}
+      />
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={email => setEmail(email)}
+        mode="outlined"
+        style={styles.input}
+      />
+      <TextInput
+        label="Password"
+        value={password}
+        onChangeText={password => setPassword(password)}
+        mode="outlined"
+        style={styles.input}
       />
       <Button
-        title="Sign Up!"
+        mode="elevated"
+        buttonColor={colors.primary}
+        textColor={colors.secondary}
         onPress={() => {
-          navigation.navigate('Interests');
+          console.log('Create handleSign-up')
         }}
-      />
+      >Sign Up!</Button>
+      <Text>Already have an account?</Text>
+      <Button
+        mode="outlined"
+        textColor={colors.secondary}
+        onPress={() => {
+          navigation.navigate('Login')
+        }}
+      >Login</Button>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -28,5 +59,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
   },
 });
