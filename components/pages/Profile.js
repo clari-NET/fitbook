fd5bf92ed7a0ec350a84d57c6efebaf4db0ac2c3
Minseok Song <minseoksong8@gmail.com>
2023-04-25 12:38:25 -0700

    implemented conditional sub-page rendering for Profile tab.

--- components/pages/Profile.js
+++ components/pages/Profile.js
@@ -1,21 +1,47 @@
-import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View, Text, StyleSheet, Button,
+} from 'react-native';
 import { useTheme } from 'react-native-paper';
+import Friends from './Friends';
 
 export default function Profile() {
   const { colors } = useTheme();
+  const [profileSubPage, setProfileSubPage] = useState('Event');
+
+  useEffect(() => {
+    // fetch data for
+    // Events
+    // Friends
+    // Community
+    // Profile
+    // fetchData(profileSubpage)
+    console.log(`${profileSubPage} was loaded`);
+  }, [profileSubPage]);
+
+  function SubPage({ page }) {
+    return { Event: <Text>Event sub-page</Text>,
+    Friends: <Friends /> }[page];
+  }
 
   return (
     <View style={[styles.container, { backgroundColor: colors.surface }]}>
-      <Text >Hello from Profile page! This is where you'll see your profile or that of another user, depending on how you got here. we love props!</Text>
+      <View>
+        <Button title='Event' onPress={() => setProfileSubPage('Event')} />
+        <Button title="Friends" onPress={() => setProfileSubPage('Friends')} />
+        <Button title="Community" onPress />
+        <Button title="Profile" onPress />
+      </View>
+      <SubPage page={profileSubPage} />
+      <Text>Hello from Profile page! This is where you'll see your profile or that of another user, depending on how you got here. we love props!</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
