6e102848cdaa713a4e25c369b08ea75a158c314d
Brett Tanonaka <tano.brett@gmail.com>
2023-04-25 14:03:02 -0700

    Removed firebaseConfig settings from ProfileSettings.js

--- components/pages/ProfileSettings.js
+++ components/pages/ProfileSettings.js
@@ -1,8 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useTheme } from 'react-native-paper';
 /* eslint-disable import/no-extraneous-dependencies */
 import { initializeApp } from 'firebase/app';
+import firebaseConfig from '../../firebaseFiles/keys';
 import {
   getFirestore,
   doc,
@@ -13,16 +14,6 @@ import {
   where,
 } from 'firebase/firestore';
 // import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDgjwdqNEegA9WfXndsYZZw0peX6M5_mw0',
-  authDomain: 'clarinet-93e57.firebaseapp.com',
-  projectId: 'clarinet-93e57',
-  storageBucket: 'clarinet-93e57.appspot.com',
-  messagingSenderId: '208590470907',
-  appId: '1:208590470907:web:dc2844679e1a12e19efa5e',
-  measurementId: 'G-BTQS94M564',
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
