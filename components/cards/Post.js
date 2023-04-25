080be9663ded3080280368e54672d6c5701a1c3a
Elliott <tung.elliott@gmail.com>
2023-04-25 14:10:41 -0700

    added post/postlist skeleton for comments

--- components/cards/Post.js
+++ components/cards/Post.js
@@ -1,39 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
-
-export default function Post() {
-  const [likes, setLikes] = useState(0);
-
-  const increaseLikes = () => {
-    setLikes(likes + 1);
-  };
-
-  return (
-    <View style={styles.postContainer}>
-      <View style={styles.postHeader}>
-        <Image
-          style={styles.profilePhoto}
-          source={{ uri: 'https://example.com/profile-photo-url.jpg' }}
-        />
-        <Text style={styles.username}>Username</Text>
-        <Text style={styles.community}>Community</Text>
-        <Text style={styles.date}>5 minutes ago</Text>
-      </View>
-      <Text style={styles.postContent}>Post content here</Text>
-      <View style={styles.postActions}>
-        <TouchableOpacity onPress={increaseLikes} style={styles.actionButton}>
-          <Text>Lift ({likes})</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.actionButton}>
-          <Text>Comment</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.actionButton}>
-          <Text>Share</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
-}
+import React, { useState } from 'react';
+import { formatDistanceToNow } from 'date-fns';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 
 const styles = StyleSheet.create({
   postContainer: {
@@ -75,3 +48,46 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
 });
+
+export default function Post({ post }) {
+  const [liked, setLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  const handleLike = () => {
+    setLiked(!liked);
+    setLikeCount(liked ? likeCount - 1 : likeCount + 1);
+  };
+
+  return (
+    <View style={styles.postContainer}>
+      <View style={styles.postHeader}>
+        <Image
+          style={styles.profilePhoto}
+          source={{ uri: 'https://source.unsplash.com/random?gym,fitness' }}
+        />
+        <Text style={styles.username}>{post.username}</Text>
+        <Text style={styles.community}>{post.community}</Text>
+        <Text style={styles.date}>
+          {formatDistanceToNow(new Date(post.date), { addSuffix: true })}
+        </Text>
+      </View>
+      <Text style={styles.postContent}>Post content here</Text>
+      <View style={styles.postActions}>
+        <TouchableOpacity onPress={handleLike} style={styles.actionButton}>
+          <Text>
+            {liked ? 'Drop' : 'Lift'}
+            (
+            {likeCount}
+            )
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.actionButton}>
+          <Text>Comment</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.actionButton}>
+          <Text>Share</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
--- components/pages/Home.js
+++ components/pages/Home.js
@@ -1,11 +1,9 @@
 import React, {useState } from 'react';
 import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Event from '../cards/Event';
-import EventList from '../lists/EventList';
-import Post from '../cards/Post';
 import { useTheme } from 'react-native-paper';
-
+import EventList from '../lists/EventList';
+import PostList from '../lists/PostList';
 
 export default function Home({ theme }) {
   const [showEvent, setShowEvent] = useState(true);
@@ -13,17 +11,37 @@ export default function Home({ theme }) {
 
   const toggleComponent = () => {
     setShowEvent(!showEvent);
-  }
+  };
+
+  const posts = [
+    {
+      id: '1',
+      profilePhotoUrl: 'https://example.com/profile-photo-url1.jpg',
+      username: 'User1',
+      community: 'Community1',
+      date: '2023-04-24T10:30:00.000Z',
+      content: 'Post content 1',
+    },
+    {
+      id: '2',
+      profilePhotoUrl: 'https://example.com/profile-photo-url2.jpg',
+      username: 'User2',
+      community: 'Community2',
+      date: '2023-04-24T11:00:00.000Z',
+      content: 'Post content 2',
+    },
+  ];
+
   return (
-    <ScrollView >
+    <ScrollView>
       <Text>Hello from Home!</Text>
-      {showEvent ? <EventList events={new Array(5).fill(0)}/> : <Post />}
+      {showEvent ? <EventList events={new Array(5).fill(0)} /> : <PostList posts={posts} />}
       <Button
         title="Toggle Post/Event"
         onPress={toggleComponent}
       />
     </ScrollView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
