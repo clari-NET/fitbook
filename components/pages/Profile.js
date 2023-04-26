cc2374fd3c9eeb52a2e5325c5ccc5e5d92a6fafc
NicMilli <nicbass2@gmail.com>
2023-04-25 18:01:31 -0700

    Profile restored

--- components/pages/Profile.js
+++ components/pages/Profile.js
@@ -1,161 +1,65 @@
-import {getAuth, updateProfile, updateEmail} from 'firebase/auth'
-import { useState, useEffect } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
-import { updateDoc, doc, collection, getDocs, query,
-        where, orderBy, deleteDoc } from 'firebase/firestore'
-import { db } from '../firebase.config'
-import { toast } from 'react-toastify'
-import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
-import homeIcon from '../assets/svg/homeIcon.svg'
-import ListingItem from '../components/ListingItem'
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { useTheme } from 'react-native-paper';
+// import getProfile from '../firebaseFiles/API';
 
-function Profile() {
-  const auth = getAuth()
-  const [loading, setLoading] = useState(true)
-  const [listings, setListings] = useState(null)
+import {
+  getFirestore,
+  getDocs,
+  collection,
+  query,
+  where,
+} from 'firebase/firestore';
+import db from '../../firebaseFiles/firebase.config';
 
-  const [changeDetails, setChangeDetails] = useState(false)
+export default function Profile() {
+  const { colors } = useTheme();
+  const [userData, setUserData] = useState([]);
+  // const details = [];
 
-  const [formData, setFormData] = useState({
-    name: auth.currentUser.displayName,
-    email: auth.currentUser.email,
-  })
-
-  const {name, email} = formData
-
-  const navigate = useNavigate()
+  async function getProfile(username) {
+    const docRef = query(collection(db, 'users'), where('username', '==', username));
+    const userInfo = await getDocs(docRef);
+    userInfo.forEach((d) => {
+      setUserData({ ...d.data(), id: d.id });
+    });
+  }
 
   useEffect(() => {
-    const fetchUserListings = async () => {
-      const listingsRef = collection(db, 'listings')
+    getProfile('test1');
+  });
 
-      const q = query(listingsRef, where('userRef', '==', auth.currentUser.uid),
-      orderBy('timestamp', 'desc')
-      )
+  // useEffect(() => {
+  //   getProfile('test1')
+  //     .then((data) => {
+  //       setUserData(data);
+  //     })
+  //     .catch((e) => console.error('error getting data', e));
+  // }, []);
 
-      const querySnap = await getDocs(q)
+  return (
+    <View style={[styles.container, { backgroundColor: colors.surface }]}>
+      <Text>Hello from Profile page! This is where you'll see your profile or that of another user, depending on how you got here. we love props!</Text>
+      <Text>
+        UserData:
+        {' '}
+        {userData ? userData.username : null}
+      </Text>
+      <Text>
+        Friends:
+        {' '}
+        {userData.friends ? userData.friends[0] : null}
+        {userData.friends ? userData.friends[1] : null}
+        {userData.friends ? userData.friends[2] : null}
+      </Text>
+    </View>
+  );
+}
 
-      let listings = []
-
-      querySnap.forEach((doc) => {
-        return listings.push({
-          id: doc.id,
-          data: doc.data()
-        })
-      })
-
-      setListings(listings)
-      setLoading(false)
-    }
-
-    fetchUserListings()
-  }, [auth.currentUser.uid])
-
-  const onLogout = () => {
-    auth.signOut()
-    navigate('/')
-  }
-
-  const onSubmit = () => {
-    const hasNameChanged = auth.currentUser.displayName !== name
-    const hasEmailChanged = auth.currentUser.email !== email
-
-    const userRef = doc(db, 'users', auth.currentUser.uid)
-    try {
-      if (hasNameChanged) {
-        updateProfile(auth.currentUser, { displayName: name })
-        updateDoc(userRef, { name })
-      }
-      if (hasEmailChanged) {
-        updateEmail(auth.currentUser, email)
-        updateDoc(userRef, { email })
-      }
-
-      toast.success('Profile updated')
-    } catch (error) {
-
-      toast.error('Could not update profile details')
-    }
-  }
-
-  const onChange = (e) =>{
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id] : e.target.value,
-    }))
-  }
-
-  const onDelete = async(listingId) => {
-    if(window.confirm('Are you sure you want to delete?')) {
-      await deleteDoc(doc(db, 'listings', listingId))
-      const updatedListings = listings.filter((listing) => listing.id !== listingId)
-      setListings(updatedListings)
-      toast.success('Successfully deleted listing')
-    }
-  }
-
-  const onEdit =(listingId) => navigate(`/edit-listing/${listingId}`)
-
-    return (
-    <div className='profile'>
-      <header className="profileHeader">
-        <p className="pageHeader">My Profile</p>
-        <button type='button'
-         onClick={onLogout} className="logOut">
-          Logout
-        </button>
-      </header>
-
-      <main>
-        <div className="profileDetailsHeader">
-          <p className="profileDetailsText">
-            Personal Details
-          </p>
-          <p className="changePersonalDetails"
-          onClick={() => {changeDetails && onSubmit()
-             setChangeDetails((prevState) => !prevState)}}>
-            {changeDetails ? 'done' : 'change'}
-          </p>
-        </div>
-
-        <div className="profileCard">
-          <form>
-            <input type="text" id='name'
-            className={!changeDetails ? 'profileName' : 'profileNameActive'}
-            disabled={!changeDetails}
-            value={name}
-            onChange={onChange} />
-            <input type="text" id='email'
-            className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
-            disabled={!changeDetails}
-            value={email}
-            onChange={onChange} />
-          </form>
-        </div>
-
-        <Link to='/create-listing' className='createListing'>
-          <img src={homeIcon} alt='home' />
-          <p>Sell or rent your home!</p>
-          <img src={arrowRight} alt='arrow right'/>
-        </Link>
-        {!loading && listings?.length > 0 ? (
-          <>
-            <p className='listingText'>Your Listings</p>
-            <ul className='listingsList'>
-              {listings.map((listing) => (
-                <ListingItem
-                  key={listing.id}
-                  listing={listing.data}
-                  id={listing.id}
-                  onDelete={() => onDelete(listing.id)}
-                  onEdit={() => onEdit(listing.id)}
-                />
-              ))}
-            </ul>
-          </>
-        ) : <p>You do not have any listings yet</p> }
-      </main>
-    </div>)
-  }
-
-  export default Profile;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
