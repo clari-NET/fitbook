8bb77acb3e15208f63ae6d1bc53dce1a458f7c1f
Johnathan Simeroth <john.simeroth@gmail.com>
2023-04-26 16:30:19 -0700

    added query to event list for future use

--- components/lists/CommunityList.js
+++ components/lists/CommunityList.js
@@ -6,31 +6,7 @@ import db from '../../firebaseFiles/firebase.config';
 import CommunityCard from '../cards/CommunityCard';
 import TextBanner from '../utility/TextBanner';
 
-const fakeDescription =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud...';
-
-const communityFakeData = [
-  {
-    name: 'Community 1',
-    description: fakeDescription,
-    banner: 'https://picsum.photos/700',
-    icon: 'U+1F6F9',
-  },
-  {
-    name: 'Community 2',
-    description: fakeDescription,
-    banner: 'https://picsum.photos/700',
-    icon: 'U+1F6F9',
-  },
-  {
-    name: 'Community 3',
-    description: fakeDescription,
-    banner: 'https://picsum.photos/700',
-    icon: 'U+1F6F9',
-  },
-];
-
-async function getCommunities(search) {
+async function getCommunities() {
   const q = query(collection(db, 'testCommunities'));
   const comDocs = await getDocs(q);
   const communities = comDocs.docs.map((doc) => doc.data());
