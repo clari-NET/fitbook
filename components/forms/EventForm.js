d969b0eadaf60cb2ca7b3941218d56b7910032bc
Minseok Song <minseoksong8@gmail.com>
2023-04-27 12:53:24 -0700

    added date form in Event creating form. Added form control. need to connect to back-end

--- components/forms/EventForm.js
+++ components/forms/EventForm.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Text, TextInput, Surface, Button } from 'react-native-paper';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import {
+  Text, TextInput, Surface, Button,
+} from 'react-native-paper';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const styles = StyleSheet.create({
   surface: {
@@ -13,26 +16,65 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
     marginBottom: 5,
   },
+  datePicker: {
+    marginHorizontal: 5,
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
-export default function PostForm({handleSubmit}) {
+export default function EventForm() {
+  const [formData, setFormData] = useState({
+    title: '',
+    description: '',
+    datetime: new Date(),
+  });
+
+  function handleCreate() {
+    // NEED TO CONNECT TO THE DB
+    // NEED TO CHANGE datetime to firestore timestamp format before POST
+    console.log(formData);
+  }
+
+  function handleFormChange(event, selectedDate, item) {
+    setFormData({
+      ...formData,
+      [item]: selectedDate,
+    });
+  }
+
   return (
     <Surface style={styles.surface}>
       <Text variant='titleLarge'>Create an Event</Text>
       <TextInput
         label='Event Title'
         mode='outlined'
-        placeholder='e.g. "Glutes wit da Homiez"'
+        placeholder='e.g. "Run with Ronnie"'
         style={styles.textInput}
+        value={formData.title}
+        onChangeText={(value) => handleFormChange(value, 'title')}
       />
       <TextInput
         label='Event Description'
         mode='outlined'
         multiline
-        placeholder='Say more, my G'
+        placeholder='e.g. "Easy run in the Golden Gate Park"'
         style={styles.textInput}
+        value={formData.description}
+        onChangeText={(value) => handleFormChange(value, 'description')}
       />
-      <Button mode='contained' onPress={handleSubmit}>Post!</Button>
+      <View style={styles.datePicker}>
+        <Text variant="labelLarge">Date:</Text>
+        <DateTimePicker
+          value={formData.datetime}
+          onChange={(event, selectedDate) => handleFormChange(event, selectedDate, 'datetime')}
+          mode='datetime'
+          minimumDate={new Date()}
+        />
+      </View>
+      <Button mode='contained' onPress={handleCreate}>Create</Button>
     </Surface>
   );
 }
