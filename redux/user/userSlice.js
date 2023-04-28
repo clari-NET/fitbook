155cf9bdec599fbd30957909fcdb5d46cc9a59b3
Minseok Song <minseoksong8@gmail.com>
2023-04-27 22:34:49 -0700

    made ProfileTab dynamic

--- redux/user/userSlice.js
+++ redux/user/userSlice.js
@@ -3,6 +3,33 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   isLoading: true,
   isSignedIn: false,
+  data: {
+    communities: [3, 13, 13],
+    community_admin: { community_id: null },
+    favoriteCommunities: [6, 13],
+    friends: [29, 10, 30, 16],
+    id: 1,
+    interests: ['Pilates',
+      'Running',
+      'Sailing',
+      'Cycling',
+      'Snowboarding',
+      'Pole Dancing',
+      'Volleyball'],
+    messages: [],
+    name: {
+      first: 'Emma',
+      last: 'Gonzalez',
+    },
+    profile_photo: 'https://randomuser.me/api/portraits/women/1.jpg',
+    settings: { dark_mode: true },
+    stats: [{
+      category: 'Basketball',
+      field: 'Points',
+      record: 31,
+    }],
+    username: 'EmmyPop',
+  }, // userSelf data
 };
 
 function changeSignIn(state = initialState, action) {
