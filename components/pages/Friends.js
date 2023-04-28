fa3cf43ae20059d71039f6ff9cdf7750f4cef6dd
Brett Tanonaka <tano.brett@gmail.com>
2023-04-28 00:01:53 -0700

    Friends grabs user data from user store

--- components/pages/Friends.js
+++ components/pages/Friends.js
@@ -2,41 +2,34 @@ import React, { useState, useEffect } from 'react';
 import {
   View, TextInput, Button,
 } from 'react-native';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { getAuth } from 'firebase/auth';
 import { doc, getDoc } from 'firebase/firestore';
 import FriendsList from '../lists/FriendsList';
 import { change } from '../../redux/conversation/conversationSlice';
-import db, { docQuery } from '../../firebaseFiles/firebase.config';
+import db from '../../firebaseFiles/firebase.config';
 
 function Friends({ navigation }) {
   const [searchQuery, setSearchQuery] = useState('');
   const [friendsList, setFriendsList] = useState([]);
   const [filteredFriends, setFilteredFriendsList] = useState([]);
   const dispatch = useDispatch();
-  const auth = getAuth();
+  const { data } = useSelector((state) => state.user);
 
   useEffect(() => {
-    // get request to fetch friends list
-    const userRef = doc(db, 'users', auth.currentUser.uid);
-    // get current user's id
-    getDoc(userRef)
-      .then((coms) => {
-        const { friends } = coms.data();
-        // get all friend's user profile based on current user's friend list
-        return Promise.all(friends.map((friendId) => {
-          const friendRef = doc(db, 'users', friendId);
-          return getDoc(friendRef)
-            .then((friend) => (
-              friend.data()
-            ));
-        }))
-          .then((friendData) => {
-            setFriendsList(friendData);
-            setFilteredFriendsList(friendData);
-          });
-      }).catch((err) => console.error(err));
+    // get all friend's user profile based on current user's friend list
+    Promise.all(data.friends.map((friendId) => {
+      const friendRef = doc(db, 'users', friendId);
+      return getDoc(friendRef)
+        .then((friend) => (
+          friend.data()
+        ));
+    }))
+      .then((friendData) => {
+        setFriendsList(friendData);
+        setFilteredFriendsList(friendData);
+      });
   }, []);
 
   const handleSearch = (query) => {
